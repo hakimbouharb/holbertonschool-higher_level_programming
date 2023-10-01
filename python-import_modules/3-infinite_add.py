b46367ef332cc045d8ffe8f1215517ff5e755974
Hakim <doctorwise@kali>
2023-10-01 16:09:59 -0400

    fix pycode style in all files

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
     sumall = 0
     for i in range(1, argc):
         sumall += int(sys.argv[i])
-    print("{}".format(sumall))
+    print("{}".format(sumall)) 
