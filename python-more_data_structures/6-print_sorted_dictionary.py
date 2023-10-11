9985e4b2e10d4a559bad5a89fa787dbedc6713ca
Hakim <doctorwise@kali>
2023-10-11 10:06:23 -0400

    print sorted dictionary

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is not None:
+
+        sorted_dictionary = dict(sorted(a_dictionary.items()))
+
+        for key, value in sorted_dictionary.items():
+            print("{}: {}".format(key, value))
+            
