282d1da275aa6e714fc757a2a5104e8851b9d0ca
Hakim <doctorwise@kali>
2023-10-11 10:07:46 -0400

    fixing print sorted dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,9 +2,6 @@
 
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None:
-
         sorted_dictionary = dict(sorted(a_dictionary.items()))
-
         for key, value in sorted_dictionary.items():
             print("{}: {}".format(key, value))
-            
