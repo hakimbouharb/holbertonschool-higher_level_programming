060b093a610fd607fd3471a2b754dbc240fd51fa
Hakim <doctorwise@kali>
2023-10-14 06:23:12 -0400

    fixing all the errors

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,11 +2,6 @@
 """Write an empty class square that defines a square"""
 
 
-class Square:
-    pass
-
-
-"""create an empty class"""
-
-my_square = Square  # initiate
-print(type(my_square))  # print square type
+class Square():
+    """empty class"""
+    None
