8247839fe24bc6a4cf336b125c4e4695845c527b
Hakim <doctorwise@kali>
2023-10-14 06:15:32 -0400

    add documentation to a file

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Write an empty class square that defines a square"""
 class Square:
     pass
+"""create an empty class"""
 
-
-my_square = Square
-print(type(my_square))
+my_square = Square """initiate """
+print(type(my_square))"""print square type"""
