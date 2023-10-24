3e293322a759abf29907c25e2632e0a59e2dd2ae
Hakim <doctorwise@kali>
2023-10-24 18:23:33 -0400

    6 base geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module base geometry """
+
+
+class BaseGeometry:
+    """ create class geometry with Public instance method: area """
+    def area(self):
+        raise Exception("area() is not implemented")
