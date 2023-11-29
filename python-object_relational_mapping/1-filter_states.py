cf0e34cf88860d3ec29b47e9c476f9e4820e998e
hakimbouharb <6869@holbertonstudents.com>
2023-11-29 05:57:17 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
+from MySQLdb import connect
+from sys import argv
+
+
+if __name__ == "__main__":
+    """conncet to db and run the query then print the result"""
+    db = connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;")
+    res = cur.fetchall()
+    for row in res:
+        print(row)
