cad2176b3c7e8f25e23e43f17d122d023d612c62
hakimbouharb <6869@holbertonstudents.com>
2023-12-01 10:29:30 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""a script that lists all State objects
-that contain the letter a from the database"""
+# Lists the State object with the name passed as argument
+# from the database hbtn_0e_6_usa.
+# Usage: ./10-model_state_my_get.py <mysql username> /
+#                                   <mysql password> /
+#                                   <database name>
+#                                   <state name searched>
+import sys
 from sqlalchemy import create_engine
-from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+from model_state import State
 
 if __name__ == "__main__":
-    """connecting to database"""
-    con = "mysql://{}:{}@localhost:{}/{}".format(
-        argv[1], argv[2], 3306, argv[3])
-    engine = create_engine(con)
-    make_session = sessionmaker(bind=engine)
-    session = make_session()
-    res = session.query(State).distinct().filter(
-        State.name.like("%{}%".format(argv[4]))).all()
-    if not res:
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
         print("Not found")
-    else:
-        for i in res:
-            print("{}".format(i.id))
