e3142ac9ad51e17088c89007413cf3bd4a62a4fe
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 <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>
2024-02-13 09:59:18 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,4 @@
+document.getElementById('update_header').addEventListener('click', function updateHeader() {
+    const header = document.querySelector('header');
+    header.textContent = 'New Header!!!';
+  });
